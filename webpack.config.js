6d1e32da994ccb83e48ef17737de01b9000d2879
Paul-js-hub <hesponpoisson@gmail.com>
2022-05-31 16:29:41 +0300

    Setup local dev server using webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,34 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: {
-        index: './src/index.js',
-        print: './src/print.js',
-      },
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  devServer: {
+    static: './dist',
+  },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
   },
-  mode: 'development'
+  optimization: {
+    runtimeChunk: 'single',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  mode: "development",
 };
